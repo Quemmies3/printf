28b8dcf07195024e22e1188c55efc55c4a6d4700
root <Quemestree@gmail.com>
2022-10-18 06:39:36 -0700

    printf

--- print_short_hex.c
+++ print_short_hex.c
@@ -7,9 +7,11 @@
  * @ibuf: index for buffer pointer
  * Return: number of chars printed
  */
+
 int prinhhex(va_list arguments, char *buf, unsigned int ibuf)
 {
 	short int int_input, i, isnegative, count, first_digit;
+
 	char *hexadecimal, *binary;
 
 	int_input = va_arg(arguments, int);
@@ -41,5 +43,6 @@ int prinhhex(va_list arguments, char *buf, unsigned int ibuf)
 	}
 	free(binary);
 	free(hexadecimal);
+
 	return (count);
 }
